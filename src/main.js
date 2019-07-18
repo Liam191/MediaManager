7025f3eae429ee52224a6ecc759843b41773ee48
Liam Hilliard <liam.h.-@hotmail.com>
2019-07-19 00:40:57 +0100

    Add basic menu

--- src/main.js
+++ src/main.js
@@ -6,12 +6,17 @@ const {
 } = require('electron');
 
 app.on('ready', createWindow);
+app.on('browser-window-created',function(event, window){
+    // Disable default menu for all new browser instances.
+    window.setMenu(null);
+});
 
 function createWindow(){
     const window = new BrowserWindow({
         width: prefs.window.width,
         height: prefs.window.height,
         title: 'Media Manager',
+        frame: false,
         webPreferences:{
             nodeIntegration: true
         }
