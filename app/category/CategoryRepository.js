cded7a87dae7642b21f93ff6809fc0b1ee6a2460
Liam191 <liam.h.-@hotmail.com>
2019-07-21 22:46:38 +0100

    Add WIP init code to repository module

--- app/category/CategoryRepository.js
+++ app/category/CategoryRepository.js
@@ -1,4 +1,19 @@
 const CategoryRepository = function(){
     "use strict";
+
+    const idb = window.indexedDB;
+
     return {};
+
+    function init(){
+        let request = idb.open('MediaDatabase', 1);
+
+        request.onerror = function(event){
+
+        };
+
+        request.onsuccess = function(event){
+
+        };
+    }
 }();
