803b24f3ef1054ad0144cdd270825d42d8e8e985
Liam Hilliard <liam.h.-@hotmail.com>
2019-07-25 01:16:03 +0100

    Open database connection

--- app/category/CategoryRepository.js
+++ app/category/CategoryRepository.js
@@ -1,7 +1,8 @@
 const CategoryRepository = function(){
     "use strict";
-
+    const _this = this;
     const idb = window.indexedDB;
+    init();
 
     return {};
 
@@ -9,11 +10,12 @@ const CategoryRepository = function(){
         let request = idb.open('MediaDatabase', 1);
 
         request.onerror = function(event){
-
+            console.log('DB connection error!', event, request);
+            _this.onError(new Error('Unable to open database!'), event, request.errorCode);
         };
 
         request.onsuccess = function(event){
-
+            console.log('DB connection opened', event, request);
         };
     }
 }();
