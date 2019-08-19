398b5487f5fd1d789551ac542301dbc09409396d
Liam Hilliard <liam.h.-@hotmail.com>
2019-08-19 20:52:37 +0100

    Comment out unused code for future reference

--- app/category/CategoryRepository.js
+++ app/category/CategoryRepository.js
@@ -1,21 +1,37 @@
 const CategoryRepository = function(){
     "use strict";
-    const _this = this;
-    const idb = window.indexedDB;
-    init();
+    // const _this = this;
+    // const idb = window.indexedDB;
+    // init();
 
     return {};
 
-    function init(){
-        let request = idb.open('MediaDatabase', 1);
+    // function init(){
+    //     let request = idb.open('MediaDatabase', 1);
 
-        request.onerror = function(event){
-            console.log('DB connection error!', event, request);
-            _this.onError(new Error('Unable to open database!'), event, request.errorCode);
-        };
+    //     request.onerror = function(event){
+    //         console.log('DB connection error!', event, request);
+    //         _this.onError(new Error('Unable to open database!'), event, request.errorCode);
+    //     };
 
-        request.onsuccess = function(event){
-            console.log('DB connection opened', event, request);
-        };
-    }
+    //     request.onsuccess = function(event){
+    //         console.log('DB connection opened', event, request);
+    //     };
+    // }
+
+    // const request = window.indexedDB.open('MediaManagerDB', 1);
+    // console.log('request: ', request);
+
+    // request.onerror = function(event){
+    //     console.log('ERROR opening database connection', event.target);
+    // };
+
+    // request.onsuccess = function(event){
+    //     console.log('success', event.target.result, request.result);
+    // };
+
+    // request.onupgradeneeded = function(e){
+    //     let db = request.result;
+    //     let store = db.createObjectStore();
+    // };
 }();
