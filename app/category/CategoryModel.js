e7c9642f6e8ef4a3e15ef5b25f0c47eb045861de
Liam Hilliard <liam.h.-@hotmail.com>
2019-08-19 20:51:28 +0100

    Remove smoke test code

--- app/category/CategoryModel.js
+++ app/category/CategoryModel.js
@@ -1,19 +1,3 @@
-const CategoryRepository = function(){
+const CategoryModel = function(require){
     "use strict";
-
-    const request = window.indexedDB.open('MediaManagerDB', 1);
-    console.log('request: ', request);
-
-    request.onerror = function(event){
-        console.log('ERROR opening database connection', event.target);
-    };
-
-    request.onsuccess = function(event){
-        console.log('success', event.target.result, request.result);
-    };
-
-    request.onupgradeneeded = function(e){
-        let db = request.result;
-        let store = db.createObjectStore();
-    };
-}();
+}(require);
